07bcc423f3e144b94f78bc18d26b81855344224a
sohamderoy <sohamderoy.iitk@gmail.com>
2021-07-11 11:49:20 +0000

    Deploying to gh-pages from  @ 18c1a9fd72f89bf91d5e45bf31d7c36b5df88e6c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.831913edfbf2f2eac426ea4f8ee8366a.js"
+  "/precache-manifest.522a72fabab32e3c4463490d42f8e2ea.js"
 );
 
 self.addEventListener('message', (event) => {
